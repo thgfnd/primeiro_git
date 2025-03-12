66fca1549e4c5fe31241cbf9278d136c9d85fc15
thgfnd <thiagosouza02@cs.cruzeirodosul.edu.br>
2025-03-12 20:13:48 -0300

    JavaScript

---
+++ kkk.js
@@ -0,0 +1,12 @@
+let isSunny = true;
+        if(isSunny){
+            console.log("Vamos ao parque!");
+        }else{
+            console.log("Vamos ficar em casa");
+        }
+
+
+        console.log(`a soma de 2 + 2 é ${2 + 2}`);
+        console.log(typeof null);
+        console.log(typeof NaN);
+        console.log(isNaN(123));
